9120e23df8cdbdcc5903dd93959b1012289f24b1
subina <subinasubi2000@gmail.com>
2022-09-14 11:36:33 +0530

    adds

--- ToDo/todoapp/views.py
+++ ToDo/todoapp/views.py
@@ -1,21 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import View
+from todoapp import forms
+from django.contrib.auth.models import User
 
 
 
 class SignUpView(View):
     def get(self,request,*args,**kwargs):
-        return render(request,"registration.html")
+        form = forms.RegistrationForm()
+        return render(request,"registration.html",{"form":form})
 
     def post(self,request,*args,**kwargs):
+        form=forms.RegistrationForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(**form.cleaned_data)
+            return redirect("signin")
 
-        print(request.POST.get("firstname"))
-        print(request.POST.get("lastname"))
         return render(request,"registration.html")
 
 class LoginView(View):
     def get(self,request,*args,**kwargs):
-        return render(request,"login.html")
+        form=forms.LoginForm()
+        return render(request,"login.html",{"form":form})
 
     def post(self,request,*args,**kwargs):
         print(request.POST.get("username"))
