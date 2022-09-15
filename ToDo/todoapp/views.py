d68d8029e86516d66c308f71b8c1bf9d92b75fa6
subina <subinasubi2000@gmail.com>
2022-09-15 11:32:37 +0530

    adds

--- ToDo/todoapp/views.py
+++ ToDo/todoapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.views.generic import View
 from todoapp import forms
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 
 
@@ -15,8 +16,8 @@ class SignUpView(View):
         if form.is_valid():
             User.objects.create_user(**form.cleaned_data)
             return redirect("signin")
-
-        return render(request,"registration.html")
+        else:
+            return render(request,"registration.html",{"form":form})
 
 class LoginView(View):
     def get(self,request,*args,**kwargs):
@@ -24,10 +25,26 @@ class LoginView(View):
         return render(request,"login.html",{"form":form})
 
     def post(self,request,*args,**kwargs):
-        print(request.POST.get("username"))
-        print(request.POST.get("password"))
+        form=forms.LoginForm(request.POST)
+        if form.is_valid():
+            uname=form.cleaned_data.get("username")
+            pwd=form.cleaned_data.get("password")
+            user=authenticate(request,username=uname,password=pwd)
+            if user:
+                login(request,user)
+                print("login success")
+                return redirect("index")
+            else:
+                print("invalid credentials")
+                return render(request,"login.html",{"form":form})
         return render(request,"login.html")
 
+class IndexView(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,"home.html")
 
 
-
+class SignOutView(View):
+    def get(self,request,*args,**kwargs):
+        logout(request)
+        return redirect("signin")
